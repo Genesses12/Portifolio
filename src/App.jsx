cd24b6215d7e2038981040e88d749e2545b783f5
genesses fernandes <genesses12@gmail.com>
2024-07-11 12:33:32 -0400

    adicioando Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 
 import About from './components/About'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 import Formacao from './components/Formacao'
 import Navbar from './components/Navbar'
 import Projects from './components/Projects'
@@ -18,6 +19,7 @@ function App() {
         <Skills /> 
         <Formacao />
         <Contact />
+        <Footer/>
         
     </main>
   );
