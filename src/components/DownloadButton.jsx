27515673aa5cc022ff6fd6909d5bb7783055d309
genesses fernandes <genesses12@gmail.com>
2024-08-11 17:31:28 -0400

    correção

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -1,7 +1,7 @@
 
 const DownloadButton = () => {
   return (
-    <a href="./curriculo.pdf" className='mr-14 mt- px-5 py-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition duration-300' download> 
+    <a href="./curriculum.pdf" className='mr-14 mt- px-5 py-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition duration-300' download> 
       <button>Baixar Curriculum</button>
     </a>
   );
