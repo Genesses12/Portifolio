8aca90933a614c520cc09945e4380e5c05a11945
genesses fernandes <genesses12@gmail.com>
2024-09-06 12:23:15 -0400

    configurando o deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/Portifolio/',
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Define o diretório de saída
+    sourcemap: true // Gera sourcemaps para debug
+  }
 })
